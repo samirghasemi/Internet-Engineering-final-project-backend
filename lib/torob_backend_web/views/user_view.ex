03c050f04d96042617aa2593f70e3cc452a9602b
Seyyed Abbas Mirghasemi <sam.s.a.mirghasemi@gmail.com>
2022-06-29 08:45:57 +0330

    init user

---
+++ lib/torob_backend_web/views/user_view.ex
@@ -0,0 +1,21 @@
+defmodule TorobBackendWeb.UserView do
+  use TorobBackendWeb, :view
+  alias TorobBackendWeb.UserView
+
+  def render("index.json", %{users: users}) do
+    %{data: render_many(users, UserView, "user.json")}
+  end
+
+  def render("show.json", %{user: user}) do
+    %{data: render_one(user, UserView, "user.json")}
+  end
+
+  def render("user.json", %{user: user}) do
+    %{
+      id: user.id,
+      username: user.username,
+      password: user.password,
+      email: user.email
+    }
+  end
+end
---
+++ priv/repo/migrations/20220628200948_create_users.exs
@@ -0,0 +1,15 @@
+defmodule TorobBackend.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :username, :string
+      add :password, :string
+      add :email, :string
+      add :avatar, :string
+
+      timestamps()
+    end
+    create unique_index(:users, [:username])
+  end
+end
